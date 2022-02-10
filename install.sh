10df000e997987403153d974c97463e0734df490
Garrett Goben <g.goben@outlook.com>
2022-02-10 17:57:06 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,4 @@ esac
 cd
 mkdir ~/fah && cd ~/fah
 wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-arm64/v7.6/fahclient_7.6.21_arm64.deb
-sudo dpkg -i --force-depends fahclient.deb
+sudo dpkg -i --force-depends fahclient_7.6.21_arm64.deb
